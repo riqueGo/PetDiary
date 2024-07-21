25bf0f757b01931e0d09d8d1403f6f3da71e2cc5
Henrique Gomes <henrique.oliveira.gomes@gmail.com>
2024-07-21 15:11:51 -0300

    navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,11 +51,14 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
+    //camera
     val camerax_version = "1.3.4"
     implementation("androidx.camera:camera-core:$camerax_version")
     implementation("androidx.camera:camera-camera2:$camerax_version")
     implementation("androidx.camera:camera-lifecycle:$camerax_version")
     implementation("androidx.camera:camera-view:$camerax_version")
     implementation("androidx.camera:camera-extensions:$camerax_version")
+
+    //schedule
 
 }
--- app/src/main/java/com/example/petdiary/ui/appointment/AddAppointmentFragment.kt
+++ app/src/main/java/com/example/petdiary/ui/appointment/AddAppointmentFragment.kt
@@ -1,4 +1,4 @@
-package com.example.petdiary.ui.schedule
+package com.example.petdiary.ui.appointment
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,31 +6,30 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.example.petdiary.R
-import com.example.petdiary.databinding.FragmentScheduleBinding
+import com.example.petdiary.databinding.FragmentAddAppointmentBinding
 
-class ScheduleFragment : Fragment() {
+class AddAppointmentFragment : Fragment() {
 
-    private var _binding: FragmentScheduleBinding? = null
-
+    private var _binding: FragmentAddAppointmentBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
-        _binding = FragmentScheduleBinding.inflate(inflater, container, false)
-
+        _binding = FragmentAddAppointmentBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.fabAddAppointment.setOnClickListener {
-            findNavController().navigate(R.id.action_scheduleFragment_to_addAppointmentFragment)
+        binding.btnCancelAppointment.setOnClickListener {
+            findNavController().navigateUp()
+        }
+
+        binding.btnSaveAppointment.setOnClickListener {
+            // Logic to save appointment
         }
     }
 
